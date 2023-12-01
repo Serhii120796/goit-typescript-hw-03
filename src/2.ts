d810a120bdb6b15a94b38c7ff988cb071dc84023
Serhii120796 <sfedyura@ukr.net>
2023-12-01 10:08:54 +0200

    hw-03

--- src/2.ts
+++ src/2.ts
@@ -35,7 +35,6 @@ class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
   constructor(name: string, department: string, salary: number) {
     super(name, department, salary + 10000);
-    this.salary = salary;
   }
 }
 
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
-const key = new Key();
+class Key {
+    signature: Math.random();
+    getSignature(): void{
+        return Math.random;
+    };
+}
 
-const house = new MyHouse(key);
-const person = new Person(key);
+class Person {
+    constructor(private key: Key) {
+    }
+    getKey(): void{
+        return key;
+    };
+}
 
-house.openDoor(person.getKey());
+abstract class House {
+    door: boolean;
+    key: Key;
+    comeIn() { 
+        // додає об'єкт класу Person у масив tenants, якщо door відкрита.
+    };
+    
+    abstract OpenDoor() {
+        // який приймає об'єкт класу Key
+     };
 
-house.comeIn(person);
+}
+
+class MyHouse extends House {
+    OpenDoor() {
+        // який приймає об'єкт класу Key
+        // Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, то двері відчиняються.
+     };
+}
+
+// Cпробуйте відтворити сценарій, в якому людина приходить додому.
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
 
 
-export {};
+export { };
